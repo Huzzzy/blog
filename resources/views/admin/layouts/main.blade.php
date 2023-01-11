8fc823ba13532556f117bc7f5caaa27cb8733f03
IlyaSushkevich <ilya.sushkevich@gmail.com>
2023-01-11 12:57:01 +0300

    Добавление в проект загрузчика файлов

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -84,6 +84,8 @@
     <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('dist/js/adminlte.js') }}"></script>
+    <!-- bs-custom-file-input -->
+    <script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
     <!-- Summernote -->
     <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
     <script>
@@ -99,6 +101,9 @@
                     ['height', ['height']]
                 ]
             });
+        });
+        $(function() {
+            bsCustomFileInput.init();
         });
     </script>
 </body>
