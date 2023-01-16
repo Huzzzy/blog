3ac7c4b6f89c39b00c18ca4b5c78750680a0387f
IlyaSushkevich <ilya.sushkevich@gmail.com>
2023-01-16 12:47:21 +0300

    Добавление хлебных крошек

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -11,8 +11,11 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.main.index') }}">Главная</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.category.index') }}">Категории</a></li>
+                            <li class="breadcrumb-item"><a
+                                    href="{{ route('admin.category.show', $category->id) }}">{{ $category->title }}</a></li>
+                            <li class="breadcrumb-item active">Редактирование категорий</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
