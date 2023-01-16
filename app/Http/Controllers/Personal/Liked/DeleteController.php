b0186d75364334bd60b3341580ed294eac29bb21
IlyaSushkevich <ilya.sushkevich@gmail.com>
2023-01-16 16:24:09 +0300

    Создание лайков

--- app/Http/Controllers/Personal/Liked/DeleteController.php
+++ app/Http/Controllers/Personal/Liked/DeleteController.php
@@ -9,11 +9,12 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class IndexController extends Controller
+class DeleteController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Post $post)
     {
-        $posts = auth()->user()->likedPosts;
-        return view('personal.liked.index', compact('posts'));
+        auth()->user()->likedPosts()->detach($post->id);
+
+        return redirect()->route('personal.liked.index');
     }
 }
