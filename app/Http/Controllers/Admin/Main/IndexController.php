50c6ef27aa1cd4030c4967edcf2f96b5fccdca9a
IlyaSushkevich <ilya.sushkevich@gmail.com>
2023-01-16 14:35:40 +0300

    Вынесение главной страницы в отдельную группу

--- app/Http/Controllers/Admin/Main/IndexController.php
+++ app/Http/Controllers/Admin/Main/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin\Main;
 
 use App\Models\Tag;
 use App\Models\Post;
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
 });
 
 Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'admin', 'verified']], function () {
-    Route::get('/', 'IndexController')->name('admin.main.index');
+
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController')->name('admin.main.index');
+    });
 
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
         Route::get('/', 'IndexController')->name('admin.category.index');
