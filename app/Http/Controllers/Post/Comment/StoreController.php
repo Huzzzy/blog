58f85291a6f5c8532d937f60fec86c925690bbd7
IlyaSushkevich <ilya.sushkevich@gmail.com>
2023-01-19 11:51:40 +0300

    Реализация добавления комментариев в постах

---
+++ app/Http/Controllers/Post/Comment/StoreController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Post\Comment;
+
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\Comment\StoreRequest;
+use App\Models\Comment;
+
+class StoreController extends Controller
+{
+    public function __invoke(Post $post, StoreRequest $request)
+    {
+        $data = $request->validated();
+        $data['user_id'] = auth()->user()->id;
+        $data['post_id'] = $post->id;
+
+        Comment::create($data);
+
+        return redirect()->route('post.show', $post->id);
+    }
+}
